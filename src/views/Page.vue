ba9f413b3e251e23c91cfaa86da11e58feb49867
Mehdi Ayache <ayache@kizeo.com>
2019-05-28 16:02:39 +0200

    init

---
+++ src/views/Page.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="page">
+        <Index/>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Index from "@/components/Index.vue";
+
+export default {
+    name: "page",
+    components: {
+        Index
+    }
+};
+</script>
